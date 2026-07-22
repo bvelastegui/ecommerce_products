f7915795b0513b6ba140d9df2a21634d8d5dec94
Bryan Velastegui <bryanslvr@gmail.com>
2026-07-22 02:12:06 -0500

    style(DataTable): reformat templates for improved readability and consistency
    - Adjusted formatting in CategoriesView, OrdersView, and PaymentsView.
    - Applied consistent alignment and spacing for DataTable components.

--- client/src/views/CategoriesView.vue
+++ client/src/views/CategoriesView.vue
@@ -130,7 +130,12 @@ async function handleSubmitOnCategoryForm(categoryData: Category) {
       </Button>
     </div>
     <div class="rounded-t-lg bg-surface-100 dark:bg-surface-800 p-1">
-      <DataTable paginator :rows="10"  :value="categoriesStore.categories" :loading="categoriesStore.loading">
+      <DataTable
+        paginator
+        :rows="10"
+        :value="categoriesStore.categories"
+        :loading="categoriesStore.loading"
+      >
         <template #empty>
           <div class="flex flex-col items-center justify-center gap-3 py-10 text-center">
             <div
--- client/src/views/PaymentsView.vue
+++ client/src/views/PaymentsView.vue
@@ -143,7 +143,12 @@ async function handleSubmitOnPaymentForm(paymentData: Payment) {
       </Button>
     </div>
     <div class="rounded-lg bg-surface-100 dark:bg-surface-800 p-4">
-      <DataTable paginator :rows="10"  :value="paymentsStore.payments" :loading="paymentsStore.loading">
+      <DataTable
+        paginator
+        :rows="10"
+        :value="paymentsStore.payments"
+        :loading="paymentsStore.loading"
+      >
         <template #empty>
           <div class="flex flex-col items-center justify-center gap-3 py-10 text-center">
             <div
